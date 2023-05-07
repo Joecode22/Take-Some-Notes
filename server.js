1101f1409448066117a82f5f05efb7dead9e4df5
josik <joe.code.mckinney@gmail.com>
2023-05-06 21:20:40 -0500

    added wildcard route to directr users to a 404 page

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
 //start the server and listen on PORT
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
